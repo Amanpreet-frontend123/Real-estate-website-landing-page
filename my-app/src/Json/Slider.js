6e938cf9a56e6551d4268571cc62e4938ad25a6f
Amanpreet <akaur.manpreet@rwit.io>
2025-06-19 16:58:31 -0700

    add project and slider page

---
+++ my-app/src/Json/Slider.js
@@ -0,0 +1,11 @@
+const slider={
+img:"../src/assets/Slider.jpg",
+vector:"../src/assets/sliderVector.png",
+divHeading:"Jacob Molen",
+divDesc:"We like the final result this project, in extraordinary and also provides the best service to the client ",
+heading:"What we have done  & what our Customers say",
+desc:"We are to help you build a excellent build, with us nothing is impossible. See what we have done and what they have to say about our work perform.",
+arrowLeft:"../src/assets/ArrowLeft.png",
+arrowRight:"../src/assets/Arrow.png"
+}
+export default slider;
---
+++ my-app/src/components/Slider.jsx
@@ -0,0 +1,22 @@
+import React from "react";
+import slider from "../Json/Slider";
+
+const Slider = () => {
+  return (
+    <>
+      <div className="flex bg-black w-[1064px] h-[566px]">
+        <div>
+          <img src={slider.img} />
+          <div className="absolute w-[263px] h-[171px] bg-white">
+            <h1>{slider.divHeading}</h1>
+            <p>{slider.divDesc}</p>
+            <img src={slider.vector} className="absolute" />
+          </div>
+        </div>
+        <div></div>
+      </div>
+    </>
+  );
+};
+
+export default Slider;
