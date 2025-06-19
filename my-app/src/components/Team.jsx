d056603a93558fa70f2f3f7ec85fb16ffdb660ee
Amanpreet <akaur.manpreet@rwit.io>
2025-06-19 13:03:06 -0700

    Add Story page

--- my-app/src/components/Team.jsx
+++ my-app/src/components/Team.jsx
@@ -5,8 +5,8 @@ import Button from './Button';
 const Team = () => {
   return (
     <>
-    <div className='flex columns-2 justify-around p-20'>
-    <div>
+    <div className='container columns-2 flex  justify-between p-20'>
+    <div >
 <img src={team.img} className='h-[540px] w-[504px]'/>
 <div className=' absolute ms-100 -mt-100 bg-black text-white p-10 place-items-center h-[294px] w-[312px]'>
   <img src={team.person.img} className='h-[100px] w-[100px] rounded-full'/>  
@@ -18,7 +18,7 @@ const Team = () => {
 <h1 className='text-[46px] box-decoration-clone max-w-100'>{team.title}</h1>
 <p className='max-w-100 text-justify text-[18px] text-gray-400'>{team.desc}</p>
 <div className='flex p-10 justify-evenly items-start'>
-<Button className="items-start">{team.teamBtn}</Button>
+<Button className="items-start ">{team.teamBtn}</Button>
 <h3 className='ps-10 m-5 text-black'>{team. footertext}</h3>
 <img src={team.sign} className='h-[16px] w-[16px] m-5'/>
 </div>
