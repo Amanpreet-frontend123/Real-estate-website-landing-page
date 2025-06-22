5317c25d89c9cee075802289244ee4eaccb57c07
Amanpreet <akaur.manpreet@rwit.io>
2025-06-22 15:22:01 -0700

    added changes in story page

--- my-app/src/components/Story.jsx
+++ my-app/src/components/Story.jsx
@@ -8,10 +8,10 @@ const Story = () => {
       <div className="container flex  ms-[120px]">
         <div className="items-start">
           <h1 className=" text-[46px]  max-w-70">{story.title}</h1>
-          <p className='max-w-100 text-justify leading-[30px] mt-[25px] text-[18px] text-gray-400'>{story.desc}</p>
+          <p className='max-w-160 text-balance leading-[50px] mt-[5px]  text-[18px] text-gray-400'>{story.desc}</p>
           <button className= "h-14 mt-[49px] bg-linear-to-r from-cyan-500 to-blue-500 text-white mb-[121px] w-50">{story.button}</button>
         </div>
-        <div><img src={story.img} className="h-[470px] w-[575px] ms-[74px] mb-[121px]"/></div>
+        <div><img src={story.img} className="h-[470px] w-[575px] mt-[40px] mb-[121px]"/></div>
       </div>
     </>
   );
