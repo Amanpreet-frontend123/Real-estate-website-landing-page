f3ea10b63e0decac5893359a8c398e8f4af131c9
Amanpreet <akaur.manpreet@rwit.io>
2025-06-22 13:01:11 -0700

    made changes in story page

--- my-app/src/components/Button.jsx
+++ my-app/src/components/Button.jsx
@@ -1,6 +1,9 @@
 import React from 'react'
 
 const Button = ({children}) => {
+
+
+    
     return (
         <>
             <button className='h-14 bg-linear-to-r from-cyan-500 to-blue-500 text-white  w-50'>
--- my-app/src/components/Story.jsx
+++ my-app/src/components/Story.jsx
@@ -5,13 +5,13 @@ import Button from "./Button";
 const Story = () => {
   return (
     <>
-      <div className="container flex justify-between  p-20 ps-100">
+      <div className="container flex  pb-[121px] ps-[120px]">
         <div className="items-start">
           <h1 className=" text-[46px] box-decoration-clone max-w-70">{story.title}</h1>
-          <p className='max-w-100 text-justify text-[18px] text-gray-400'>{story.desc}</p>
-          <Button className="p-10">{story.button}</Button>
+          <p className='max-w-100 text-justify mt-[24px] text-[18px] text-gray-400 leading-[40px]'>{story.desc}</p>
+          <button className="h-14 mt-[49px] bg-linear-to-r from-cyan-500 to-blue-500 text-white  w-50">{story.button}</button>
         </div>
-        <div><img src={story.img} className="h-[470px] w-[575px]"/></div>
+        <div><img src={story.img} className="h-[470px] w-[575px] ms-[74px]"/></div>
       </div>
     </>
   );
