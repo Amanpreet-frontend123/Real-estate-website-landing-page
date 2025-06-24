825b4f6ec060f7bf108083e9242afb53f2102762
Amanpreet <akaur.manpreet@rwit.io>
2025-06-24 10:49:55 -1100

    change

--- my-app/src/components/Card.jsx
+++ my-app/src/components/Card.jsx
@@ -5,15 +5,13 @@ const Card = ({img,title,desc,page}) => {
    const imageStyle = page === 'article'
     ?  {width:"368px" ,height:"287px"  } 
     :  {width:"64px", height:"64px" }
-const Card = ({img,title,desc,page}) => {
 
-   const imageStyle = page === 'article'
-    ?  {width:"368px" ,height:"287px"  } 
-    :  {width:"64px", height:"64px" }
+
+   
   return (
 <>
 <div className='container p-10'>
-    <img className='h-[287px] w-[368px] bg-blue-500' src={img}/>
+    <img className='h-[287px] w-[368px] bg-blue-500' src={img} style={imageStyle}/>
     <h2 className='font-bold max-w-100 text-balance'>{title}</h2>
     <p className='text-gray-500 max-w-100'>{desc}</p>
 </div>
