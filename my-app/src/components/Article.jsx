22abefb92035af9ba06f3fb66850a9f0b1a0be6c
Amanpreet <akaur.manpreet@rwit.io>
2025-06-24 17:00:04 -1100

    added changes in article page

--- my-app/src/components/Article.jsx
+++ my-app/src/components/Article.jsx
@@ -3,31 +3,26 @@ import article from "../Json/Article";
 import Card from "../components/Card";
 
 const Article = () => {
-
-
   return (
     <>
-    <div className=" container mx-auto ms-[120px] mb-[95px]">
-      <h1 className=" text-[46px] font-[600px] mb-12 ">
-        {article.title}
-      </h1>
+      <div className=" container mx-auto ms-[120px] mb-[95px]">
+        <h1 className=" text-[46px] font-[600px] mb-12 ">{article.title}</h1>
 
-      <div className=" flex container gap-[48px]">
-        {article.data.map((item) => (
-          <Card 
-            key={item.id}
-            img={item.img}
-            title={item.title}
-            desc={item.desc}
-             page="article"
-          />
-        ))}
+        <div className=" flex container gap-[48px]">
+          {article.data.map((item) => (
+            <Card
+              key={item.id}
+              img={item.img}
+              title={item.title}
+              desc={item.desc}
+              page="article"
+              titleSize="art"
+            />
+          ))}
+        </div>
       </div>
-    </div>
     </>
   );
 };
 
 export default Article;
-
-
--- my-app/src/components/Card.jsx
+++ my-app/src/components/Card.jsx
@@ -1,16 +1,19 @@
 import React from 'react'
 
-const Card = ({img,title,desc,page}) => {
+const Card = ({img,title,desc,page,titleSize}) => {
 
    const imageStyle = page === 'article'
     ?  {width:"368px" ,height:"287px"  } 
     :  {width:"64px", height:"64px" }
+
+const textStyle=titleSize==="art"?{className:"font-[600px] text-[16px]"}:{className:"font-[600px] text-[24px]"}
+    
   return (
 <>
 <div className='-mb-10 -ms-10 '>
   
-    <img className='ms-[48px] me-[50px] ' src={img} style={imageStyle}/>
-    <h2 className='font-bold ms-[48px] mt-[38px] max-w-80 mt-[21px] font-[600px] text-[28px] '>{title}</h2>
+    <img className="ms-[48px] me-[50px]" src={img} style={imageStyle} />
+    <h2 className='font-bold ms-[48px] mt-[38px] max-w-80 mt-[21px]' style={textStyle}>{title}</h2>
     <p className='text-[#C4C4C4] ms-[48px] mt-[37px] mt-[25px] leading-[40px] w-[333px]'>{desc}</p>
 </div>
 </>
