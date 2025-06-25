f5557a442b46e10178203583a1a6a1601683bd36
Amanpreet <akaur.manpreet@rwit.io>
2025-06-25 12:14:25 -1100

    changes

--- my-app/src/components/Navbar.jsx
+++ my-app/src/components/Navbar.jsx
@@ -3,9 +3,9 @@ import React from "react";
 const Navbar = () => {
   return (
     <>
-      <div className="flex justify-between bg-linear-to-r from-[#222222] to-[#474747] text-white pt-[48px]">
-        <div className="container mt-[20px]">
-          <div className=" -ms-[130px] items-start  ">
+      <div className=" flex justify-between bg-linear-to-r from-[#222222] to-[#474747] text-white pt-[38px]">
+        <div className=" container mt-[20px]">
+          <div className="ms-1 items-start  ">
             <h4 className="text-[18px] font-[700]">General</h4>
             <h1 className="text-[24px] font-[700] text-[#0A72AD]">Project</h1>
           </div>
